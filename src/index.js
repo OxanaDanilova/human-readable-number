845efb3f33219d1664b590ca116f34516a076dd7
OxanaDanilova <oxanadanilova11@gmail.com>
2021-03-07 21:39:01 +0100

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 module.exports = function toReadable (number) {
-  
+  const newArr = String(number).split('');
+  let result;
+  const firstArr = ['','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const secondArr = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const thirdArr = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  if (newArr.length === 1) {
+      if (+newArr[0]===0) {
+          result = 'zero';
+      } else {
+        result = firstArr[number];
+
+      }
+    
+  }
+  if (newArr.length === 2 && +newArr[0]!==1) {
+      result = secondArr[newArr[0]-1];
+      if (+newArr[1]!==0) {
+        result +=' '+firstArr[newArr[1]];
+    };
+} 
+  if (newArr.length === 2 && +newArr[0]===1) {
+      if (+newArr[1]===0) {
+          result = 'ten';
+      } else {
+        result = thirdArr[newArr[1]-1];
+
+      };
+    
+}
+
+  if (newArr.length === 3 && +newArr[1]!==1 && +newArr[0]!==0) {
+    result = firstArr[newArr[0]]+' '+'hundred'+' '+secondArr[newArr[1]-1];
+    if (+newArr[2]!==0) {
+        result +=' '+firstArr[newArr[2]];
+    }
+}
+if (newArr.length === 3 && +newArr[1]===1) {
+    result = firstArr[newArr[0]]+' '+'hundred'
+    if (+newArr[2]===0){
+    result += ' ten';
+    } else {
+        result += ' '+thirdArr[newArr[2]-1];
+     };
+        
+    }
+
+    if (newArr.length === 3 && +newArr[1]===0) {
+        result = firstArr[newArr[0]]+' '+'hundred';
+        if (+newArr[2]!==0) {
+            result += ' '+firstArr[newArr[2]];
+        }
+    }
+  return result;
 }
